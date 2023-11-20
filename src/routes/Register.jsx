3ae60e2d95440aa36e724829e6607ef78079668c
Jhonjandrysdev <jhonjandrysdramirez@unicesar.edu.co>
2023-11-20 16:26:14 -0500

    se retiraron componentes de login y register

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -100,11 +100,6 @@ const Register = () => {
           
             <Button text="Ingresar" type="submit" loading={loading}/>
           
-          <Links
-            to="/"
-            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-96 flex justify-center"
-            text="Ya tienes una cuenta? Ingresa"
-            />
             </div>
       </form>
     </>
